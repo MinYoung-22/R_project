ca351fac4737d21287627d55e2917947f583216b
minyoungshin <ericashin1@g.skku.edu>
2023-03-28 20:09:34 +0900

    통계입 2주차 코드 짜보기

--- statistical computing/2nd.R
+++ statistical computing/2nd.R
@@ -1,7 +1,23 @@
 #gaussian elimination medthod with R
 
-A = matrix(c(4,2,8,6,-2,3,3,5,-6))
+A = matrix(c(4,2,8,6,-2,3,3,5,-6), ncol = 3)
 b = c(25,13,-4)
 
-##gaussian elimination method
+solve(A,b)
+
+#Back Subtitution
+A = matrix(c(1,0,0,3/2,1,0,3/4,4/3,1), ncol = 3)
+b = c(25/4, 6,3)
+backsolve(A,b)
+
+#Gaussian elimination method + round-off error
+A = matrix(c(0.00001,1,2,1), ncol=2)
+b = c(2,2)
+Ab = cbind(A,b)
+solve(A,b)
+#해결방법
+options(digits=3)
+A = matrix(c(0.00001, 1,2,1), ncol =2)
+b = c(2,2)
+Ab = cbind(A,b)
 solve(A,b)
