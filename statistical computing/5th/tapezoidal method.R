b0e12b39135aa65c28ecce3e54bcb045e6c28504
minyoung22 <ericashin1@g.skku.edu>
2023-04-03 11:42:04 +0900

    pg. 8

--- statistical computing/5th/tapezoidal method.R
+++ statistical computing/5th/tapezoidal method.R
@@ -1,15 +1,32 @@
-Trapezodal = function(f, a, b,n, print= True){
+Trapezodal = function(f, a, b,n, print = TRUE ){
+  integral = 0
   if( n <= 0){
     return("choose a positive integer n!")}
   x0 = sort(c(a,b))[1]; xn = sort(c(a,b))[2]
   h =(xn - x0)/n
   
   inegeral = 0 
-  for (i in 1:n{ 
+  for (i in 1:n) { 
     integral = integral + 
-      o.5 *h*(f(x0+i*h)+f(x0+(i-1)*h))
+      0.5 *h*(f(x0+i*h)+f(x0+(i-1)*h))
   }
-  if(print == True){
+  if(print == TRUE){
   print(paste("Trapezoidal gives", integral))}
   return(integral = integral)
 }
+
+
+func_y = function(x){exp(-0.5*x^2)/sqrt(2*pi)}
+Trapezodal(func_y, -2,4, 10)
+
+int_func_y = function(x){pnorm(x)}
+int_func_y(4) - int_func_y(-2)
+
+n_set = seq(5,100, by= 5)
+result1 = sapply(n_set, Trapezodal, f = func_y, a = -2, 
+                 b = 4, print = FALSE) - 
+  (int_func_y(4) - int_func_y(-2))
+result1
+
+
+plot(n_set, abs(result1), main = "n vs Error", xlab = "n")
